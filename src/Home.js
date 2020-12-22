2a7f6990e5bc5f199f5b8e10024b09d55c9efd94
kim jae hun <zg788990@gmail.com>
2020-12-22 17:37:19 +0900

    amazon-header

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonMusic/2020/ACQ/Gateway/HolidayNonPromo/DV5/US-EN_100120_HOLNonPromo_ACQ_GW_Hero_D_3000x1200_CV4_2._CB415751492_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsP8CYF16K28SouedJz2vVIW9nr4G-_oE",
+  authDomain: "challenge-d691f.firebaseapp.com",
+  projectId: "challenge-d691f",
+  storageBucket: "challenge-d691f.appspot.com",
+  messagingSenderId: "441870070841",
+  appId: "1:441870070841:web:5d56f38b17b0d66b6c09e8",
+  measurementId: "G-2WMYR4H851"
+};
